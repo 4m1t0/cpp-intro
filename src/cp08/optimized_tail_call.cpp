f6a80e07aa8748794941ba35722738f33049a53b
4m1t0 <fum1t0.eng@gmail.com>
2019-12-31 17:51:26 +0900

    add main

--- src/cp08/optimized_tail_call.cpp
+++ src/cp08/optimized_tail_call.cpp
@@ -1,16 +1,22 @@
-int factorial( int n )
+int factorial(int n)
 {
   int temp = n;
 
-  loop:
-    if ( n < 1 )
-      return 0;
-    else if ( n == 1 )
-      return temp;
-    else
-    {
-      n = n-1;
-      temp *= n;
-      goto loop;
-    }
+loop:
+  if (n < 1)
+    return 0;
+  else if (n == 1)
+    return temp;
+  else
+  {
+    n = n - 1;
+    temp *= n;
+    goto loop;
+  }
+}
+
+int main()
+{
+  int i = factorial(5);
+  std::cout << i;
 }
--- src/cp08/tail_call.cpp
+++ src/cp08/tail_call.cpp
@@ -1,9 +1,15 @@
-int factorial( int n )
+int factorial(int n)
 {
-  if ( n < 1 )
+  if (n < 1)
     return 0;
-  else if ( n == 1 )
+  else if (n == 1)
     return 1;
   else
-    return n * factorial(n-1);
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+  int i = factorial(5);
+  std::cout << i;
 }
